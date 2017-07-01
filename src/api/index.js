02ae24e9ff31a44cd4417b08bc079f7dcfbf5e73
huangwx <hwx.simle@gmail.com>
2017-07-01 20:17:39 +0800

    project scaffold is ok

---
+++ src/api/index.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
+
+export default {
+
+}
---
+++ src/pages/Home.vue
@@ -0,0 +1,19 @@
+<style lang="stylus">
+
+</style>
+
+
+<template lang="pug">
+#Home
+  hello
+</template>
+
+
+<script>
+import Hello from '@/components/Hello'
+export default {
+  components: {
+    Hello
+  }
+}
+</script>
